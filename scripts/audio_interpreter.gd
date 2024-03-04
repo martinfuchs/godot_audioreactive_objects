cd5c12ab779aa575f92f572ce9ba3ffcdc3bdfa7
Martin Fuchs <mrtnfchs@gmail.com>
2024-03-04 19:17:14 +0100

    3d text

--- scripts/audio_interpreter.gd
+++ scripts/audio_interpreter.gd
@@ -2,9 +2,13 @@ extends Node3D
 
 @export var index = 0
 @export var smoothing = 0.1
-@export var c_rotate:Vector3 = Vector3(0,0,0)
-@export var c_move:Vector3 = Vector3(0,0,0)
-@export var c_scale:Vector3 = Vector3(0,0,0)
+@export var _rotate:Vector3 = Vector3(0,0,0)
+@export var _rotate_continuous = false
+@export var _move:Vector3 = Vector3(0,0,0)
+@export var _move_continuous = false
+@export var _move_bounds_max:Vector3 = Vector3(0,0,0)
+@export var _move_bounds_min:Vector3 = Vector3(0,0,0)
+@export var _scale:Vector3 = Vector3(0,0,0)
 
 # fft spectrum helps us identify the different frequencies present in the signal. 
 @onready var spectrum = Spectrum.new()
@@ -17,7 +21,19 @@ func _ready():
 
 func _process(delta):
 	spectrum.calculate()
-	position = c_move*spectrum.get_energy_at(index) #input 0-16 according to defined vu_count, returns value between 0 and 1
-	rotation = c_rotate*spectrum.get_energy_at(index)
-	scale = Vector3(1,1,1)+c_scale*spectrum.get_energy_at(index)
+	if _move_continuous:
+		position += _move*spectrum.get_energy_at(index)
+		if position>_move_bounds_max:
+			position = _move_bounds_min
+		if position<_move_bounds_min:
+			position = _move_bounds_max
+	else:
+		position = _move*spectrum.get_energy_at(index) #input 0-16 according to defined vu_count, returns value between 0 and 1
+	
+	if _rotate_continuous:
+		rotation += _rotate*spectrum.get_energy_at(index)
+	else:
+		rotation = _rotate*spectrum.get_energy_at(index)
+		
+	scale = Vector3(1,1,1)+_scale*spectrum.get_energy_at(index)
 
