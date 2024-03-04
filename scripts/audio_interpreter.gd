6c4984f3fc9fa85ed9009444216f305b044f8b09
Martin Fuchs <fuchs@tweaklab.org>
2024-03-04 17:33:05 +0100

    audio object transform

--- scripts/audio_interpreter.gd
+++ scripts/audio_interpreter.gd
@@ -1,15 +1,22 @@
 extends Node3D
 
+@export var index = 0
+@export var smoothing = 0.1
+@export var c_rotate:Vector3 = Vector3(0,0,0)
+@export var c_move:Vector3 = Vector3(0,0,0)
+@export var c_scale:Vector3 = Vector3(0,0,0)
+
 @onready var spectrum = Spectrum.new()
 
 func _ready():
 	spectrum.vu_count = 16
-	spectrum.smoothing = 0.4
+	spectrum.smoothing = smoothing
 	spectrum.init()
 
 
 func _process(delta):
 	spectrum.calculate()
-	position.y = spectrum.get_energy_at(0) #input 0-16 according to defined vu_count, returns value between 0 and 1
-	rotation.x += spectrum.get_energy_at(2)*0.01
+	position = c_move*spectrum.get_energy_at(index) #input 0-16 according to defined vu_count, returns value between 0 and 1
+	rotation = c_rotate*spectrum.get_energy_at(index)
+	scale = Vector3(1,1,1)+c_scale*spectrum.get_energy_at(index)
 
